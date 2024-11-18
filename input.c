4c71d280f91dd40eda40412368ecb1431b8d4bda
Patrickdh04 <163329805+Patrickdh04@users.noreply.github.com>
2024-11-18 15:02:32 +0100

    Update input.c

--- input.c
+++ input.c
@@ -2,6 +2,18 @@
 
 int main(void){
 
+    printf("Velkommen til vores Movie Recommender! Nu vil vi gerne have noget information fra dig :)\n");
+    char *genre1 = '?';
+    int antalgenre = 9;
+    do {
+    printf("Indtast genre 1 du gerne vil se (Skriv ? for liste af genre):");
+    scanf(" %s", &genre1);
+    for (int i = 0; i > ; i++){
+        // Lav liste over alle genre
+    }
+
+    } while (genre1 != '?');
+
 
     return 0;
 }
