c50071885c5b391d470c838e80661337c67b75b6
Goat King <joaki2003@gmail.com>
2024-12-05 00:23:11 +0100

    More progress on movieSort.c
    - Added the genre "Documentary" to input.c
    - Fixed all errors when saving data from the csv file
    - The program will now discard movies that do not have duration or rating
    - It is now possible to search for a specific movie/series in relation to their name
    - If a movie/series is found during search, all its information is saved

--- MovieSort_JPG/movieSort.c
+++ MovieSort_JPG/movieSort.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NAMEMAXLENGTH 50
-#define GENREMAXLENGTH 100
+#define NAMEMAXLENGTH 100
+#define GENREMAXLENGTH 12
 #define AGERATINGMAXLENGTH 10
 #define MAXDURATION 4
 #define MAXAMOUNTOFGENRES 5
 #define ACTORSMAXLENGTH 200
-#define MOVIESTOTAL 20
+#define MOVIESTOTAL 9957
 #define TIMEMAXLENGTH 4
-#define YEARMAXLENGTH 11
+#define YEARMAXLENGTH 13
 #define MULTIPLEGENRESMAXLENGTH 500
 #define DURATIONMAXLENGTH 10
 
@@ -25,31 +25,70 @@ typedef struct movieData
     char actors[ACTORSMAXLENGTH];
 } movieData;
 
+typedef struct pref
+{
+    char genre1[GENREMAXLENGTH];
+    int genrepref1;
+    char genre2[GENREMAXLENGTH];
+    int genrepref2;
+    char genre3[GENREMAXLENGTH];
+    int genrepref3;
+    int min;
+    int max;
+    int timetowatch;
+} pref;
+
+typedef struct conpref
+{
+    char seriesName[NAMEMAXLENGTH];
+    int timetowatch;
+} conpref;
+
 // Prototypes
-void saveMovieData();
+void saveMovieData(movieData *moviesPreSort);
+void discard(movieData *movieToDiscard);
+void sortMovies(movieData *moviesPreSort, movieData *movies, movieData *series, char nc, pref newMovie, char *conWatch, movieData *conWatchData);
 
 int main(void)
 {
-    printf("Not overflow!\n");
-    movieData movies[MOVIESTOTAL];
-    movieData series[MOVIESTOTAL];
+    // temp input
+    char nc = 'n';
+    pref newMovie = {"Adventure", 2, "yo", 1, "", 3, 2, 8, 10000};
+    conpref conWatch = {"Attack on Titan", 10000};
+    movieData conWatchData;
 
-    saveMovieData();
+    // As a lot of data has to be stored, we will save it using dynamic memory allocation
+    movieData *moviesPreSort = malloc(MOVIESTOTAL * sizeof(movieData));
+
+    movieData *movies = malloc(MOVIESTOTAL * sizeof(movieData));
+    movieData *series = malloc(MOVIESTOTAL * sizeof(movieData));
+
+    saveMovieData(moviesPreSort);
+    sortMovies(moviesPreSort, movies, series, nc, newMovie, conWatch.seriesName, &conWatchData);
+    if (nc == 'c' && (strcmp(conWatchData.title, conWatch.seriesName) == 0))
+    {
+        printf("Name: %s, isSeries: %i, ageRating: %s, duration: %i, g1: %s, g2: %s, g3: %s, g4: %s, g5: %s, rating: %lf\n", conWatchData.title, conWatchData.isSeries, conWatchData.ageRating, conWatchData.duration, conWatchData.genre[0], conWatchData.genre[1], conWatchData.genre[2], conWatchData.genre[3], conWatchData.genre[4], conWatchData.rating);
+    }
+    else if (nc == 'c')
+    {
+        printf("Movie/series not found.\n");
+    }
+
+    free(moviesPreSort);
+    free(movies);
+    free(series);
 
     return 0;
 }
 
-void saveMovieData()
+void saveMovieData(movieData *moviesPreSort)
 {
-    movieData moviesPreSort[MOVIESTOTAL];
-
     // To determine if a movie is a series or not, we will look at if its release date has a "-" in the name after the number
     // This would suggest that a movie is released over the course of more than a year, which is only possible for tv-series
     // We therefore save the year information in a string, which we will later use to determine if a movie is a series or not
     char year[YEARMAXLENGTH];
     // We save all genres in one string to begin with, as the amount of genres varies between the data
     char genres[MULTIPLEGENRESMAXLENGTH];
-    char duration[DURATIONMAXLENGTH] = "0";
     FILE *pFile = fopen("movies.csv", "r");
 
     if (pFile == NULL)
@@ -60,21 +99,42 @@ void saveMovieData()
 
     for (int i = 0; i < MOVIESTOTAL; i++)
     {
-        fscanf(pFile, " %49[^,], %10[^,], %9[^,], %9[^,],", moviesPreSort[i].title, year, moviesPreSort[i].ageRating,
-               duration);
+        char stringduration[DURATIONMAXLENGTH] = "0";
+        // As all data is split up via comma, any movie with a comma in its name has the name in between "|"
+        // We therefore check for this so that those movies' data do not become invalid
+        char commaInNameCheck[NAMEMAXLENGTH] = "0";
+        fscanf(pFile, " %c", commaInNameCheck);
+        if (commaInNameCheck[0] == '|')
+        {
+            fscanf(pFile, " %99[^|]|,", moviesPreSort[i].title);
+        }
+        else
+        {
+            fscanf(pFile, " %99[^,],", moviesPreSort[i].title);
+            strcat(commaInNameCheck, moviesPreSort[i].title);
+            strcpy(moviesPreSort[i].title, commaInNameCheck);
+        }
+
+        fscanf(pFile, " %12[^,], %9[^,], %9[^,],", year, moviesPreSort[i].ageRating,
+               stringduration);
         // We check if a movie is a series
         moviesPreSort[i].isSeries = (year[5] == '-');
 
-        // If there is a valid duration, aka if it not left empty in the dataset, it will be saved
-        if (duration[0] != '0')
+        // If there is a valid duration we save the data, if not we discard the movie
+        if (stringduration[0] != '0')
         {
-            sscanf(duration, "%i", &moviesPreSort[i].duration);
+            sscanf(stringduration, "%i", &moviesPreSort[i].duration);
         }
-
+        else
+        {
+            fscanf(pFile, " %*[^\n]");
+            discard(&moviesPreSort[i]);
+            continue;
+        }
         // Before we continue saving data we need to determine if there are multiple genres or not
         // If there are more than one, the genres will be listed within this "|" symbol
         // If there is just one, the genre data will just be between comma like the rest of the data
-        char multipleGenreCheck[GENREMAXLENGTH];
+        char multipleGenreCheck[GENREMAXLENGTH] = "0";
         fscanf(pFile, " %c", multipleGenreCheck);
         // As we need to save a value on all 5 genre variables of the struct, we will start by manually saving a value
         // indicating that it is empty
@@ -87,12 +147,12 @@ void saveMovieData()
         {
             fscanf(pFile, " %499[^|]|,", genres);
             // We save each genre
-            sscanf(genres, " %100[^,], %100[^,], %100[^,], %100[^,], %100[^|]", moviesPreSort[i].genre[0],
-                                        moviesPreSort[i].genre[1], moviesPreSort[i].genre[2], moviesPreSort[i].genre[3], moviesPreSort[i].genre[4]);
+            sscanf(genres, " %19[^,], %19[^,], %19[^,], %19[^,], %19[^|]", moviesPreSort[i].genre[0],
+                   moviesPreSort[i].genre[1], moviesPreSort[i].genre[2], moviesPreSort[i].genre[3], moviesPreSort[i].genre[4]);
         }
-        else
+        else if (multipleGenreCheck[0] != ',')
         {
-            fscanf(pFile, " %99[^,],", moviesPreSort[i].genre[0]);
+            fscanf(pFile, "%99[^,],", moviesPreSort[i].genre[0]);
             strcat(multipleGenreCheck, moviesPreSort[i].genre[0]);
             strcpy(moviesPreSort[i].genre[0], multipleGenreCheck);
         }
@@ -103,6 +163,8 @@ void saveMovieData()
         fscanf(pFile, " %c", ratingCheck);
         if (ratingCheck[0] == ',')
         {
+            fscanf(pFile, " %*[^\n]");
+            discard(&moviesPreSort[i]);
             continue;
         }
         fscanf(pFile, " %2[^,]", &ratingCheck[1]);
@@ -117,6 +179,50 @@ void saveMovieData()
         fscanf(pFile, " %*[^\n]");
     }
     fclose(pFile);
+    return;
+}
+
+void discard(movieData *movieToDiscard)
+{
+    // By setting the title of a movie to "0" we can deem it as discarded and ignore it later when recommending movies
+    strcpy(movieToDiscard->title, "0");
+}
+
+void sortMovies(movieData *moviesPreSort, movieData *movies, movieData *series, char nc, pref newMovie, char *conWatch, movieData *conWatchData)
+{
+    if (nc == 'n')
+    {
+        for (int i = 0; i < MOVIESTOTAL; i++)
+        {
+            // Check for search criteria here
+        }
+    }
+    else if (nc == 'c') // If user continues watching the movie name will be searched for
+    {
+        for (int i = 0; i < MOVIESTOTAL; i++)
+        {
+            if (strcmp(moviesPreSort[i].title, conWatch) == 0)
+            {
+                // If movie/series is found, all information is saved in conWatchData
+                strcpy(conWatchData->title, conWatch);
+                conWatchData->isSeries = moviesPreSort[i].isSeries;
+                strcpy(conWatchData->ageRating, moviesPreSort[i].ageRating);
+                conWatchData->duration = moviesPreSort[i].duration;
+                strcpy(conWatchData->genre[0], moviesPreSort[i].genre[0]);
+                strcpy(conWatchData->genre[1], moviesPreSort[i].genre[1]);
+                strcpy(conWatchData->genre[2], moviesPreSort[i].genre[2]);
+                strcpy(conWatchData->genre[3], moviesPreSort[i].genre[3]);
+                strcpy(conWatchData->genre[4], moviesPreSort[i].genre[4]);
+                conWatchData->rating = moviesPreSort[i].rating;
+                break;
+            }
+        }
+    }
+    else
+    {
+        printf("Error: No command was found");
+        exit(EXIT_FAILURE);
+    }
 
     return;
 }
--- TimeInput_P/input.c
+++ TimeInput_P/input.c
@@ -3,11 +3,8 @@
 #include <time.h>
 #include <string.h>
 
-struct pref new_rec(void);
-struct conpref con_rec(void);
-int getdiff(void);
-
-struct pref {
+typedef struct pref
+{
     char genre1[10];
     int genrepref1;
     char genre2[10];
@@ -17,41 +14,54 @@ struct pref {
     int min;
     int max;
     int timetowatch;
-};
+} pref;
 
-struct conpref {
-    char series[50];
+typedef struct conpref
+{
+    char seriesName[50];
     int timetowatch;
-};
+} conpref;
 
-int main(void){
+//Prototypes
+pref new_rec(void);
+conpref con_rec(void);
+int getdiff(void);
+
+int main(void)
+{
 
     char nc = '?';
 
-    do { // New recommendation or continue with series?
+    do
+    { // New recommendation or continue with series?
         printf("Do you want a new recommendation or continue watching a series? (n for new / c for continue): \n");
         scanf(" %c", &nc);
     } while (nc != 'n' && nc != 'c'); // check if user typed n or c
 
-    if (nc == 'n'){
-        struct pref preference = new_rec(); // run new recommendation data collection
-    } else if (nc == 'c'){
-        struct conpref series = con_rec(); // run continue series data collection
-    } else { // if somehow nc is not n or c
+    if (nc == 'n')
+    {
+        pref preference = new_rec(); // run new recommendation data collection
+    }
+    else if (nc == 'c')
+    {
+        conpref series = con_rec(); // run continue series data collection
+    }
+    else
+    { // if somehow nc is not n or c
         printf("Problem with program, stopping...");
         exit(EXIT_FAILURE);
     }
 
-
     return 0;
 }
 
-struct pref new_rec(void){
+pref new_rec(void)
+{
 
-    int g1 = -1, g2 = -1, g3 = -1; // g1-2-3 default index -1
+    int g1 = -1, g2 = -1, g3 = -1;                    // g1-2-3 default index -1
     char gp1[10] = "?", gp2[10] = "?", gp3[10] = "?"; // 3 genre preferences
-    char genre[16][10] = {"Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Drama", "Family",
-                          "Fantasy", "History", "Horror", "Mystery", "Romance", "Sci-Fi", "Sport", "Thriller"};
+    char genre[17][12] = {"Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Drama", "Family",
+                          "Fantasy", "History", "Horror", "Mystery", "Romance", "Sci-Fi", "Sport", "Thriller", "Documentary"};
 
     char ar;
 
@@ -59,44 +69,55 @@ struct pref new_rec(void){
 
     printf("\nWelcome to the Software 1 Group 6 Movie recommender!\n\n");
 
-    do { // Get 3 genres
+    do
+    { // Get 3 genres
         printf("\nWrite 1 to 3 genres you want to see, separated by a comma (,) (Write '?' for a list of genres): \n");
         scanf(" %9s, %9s, %9s", &gp1, &gp2, &gp3);
 
-        for (int i = 0; i < 16; i++){
-            if (strcmp(gp1, genre[i]) == 0){
+        for (int i = 0; i < 17; i++)
+        {
+            if (strcmp(gp1, genre[i]) == 0)
+            {
                 g1 = i;
-            } else if (strcmp(gp2, genre[i]) == 0) {
+            }
+            else if (strcmp(gp2, genre[i]) == 0)
+            {
                 g2 = i;
-            } else if (strcmp(gp3, genre[i]) == 0) {
+            }
+            else if (strcmp(gp3, genre[i]) == 0)
+            {
                 g3 = i;
-            } // Get the 16 genre indexes 0-15
+            } // Get the 17 genre indexes 0-16
         }
 
-        if (strcmp(gp1, "?") == 0) { // print list of genres
-            printf("\nAction, Adventure, Animation, Biography, Comedy, Crime, Drama, Family, \nFantasy, History, Horror, Mystery, Romance, Sci-Fi, Sport, Thriller\n");
+        if (strcmp(gp1, "?") == 0)
+        { // print list of genres
+            printf("\nAction, Adventure, Animation, Biography, Comedy, Crime, Drama, Family, \nFantasy, History, Horror, Mystery, Romance, Sci-Fi, Sport, Thriller, Documentary");
         }
 
     } while (g1 == -1); // repeat if no valid genre 1
 
-    do {
+    do
+    {
         printf("\nDo you want to be recommended 18+ films? (y/n): \n");
         scanf(" %c", &ar);
     } while (ar != 'y' && ar != 'n'); // repeat if no valid answer
 
-    do {
+    do
+    {
         printf("\nWrite the minimum IMDb rating you want to look for (whole number between 1 and 9): \n");
         scanf(" %d", &min_rating);
     } while (min_rating < 0 || min_rating > 10); // repeat if not between 1 and 9
 
-    do {
+    do
+    {
         printf("\nWrite the maximum IMDb rating you want to look for (whole number between 1 and 10): \n");
         scanf(" %d", &max_rating);
     } while (max_rating <= min_rating || max_rating > 10); // repeat if not bigger than min_rating and less than 10
 
     int diff = getdiff(); // get time diff in seconds
 
-    struct pref preference;
+    pref preference;
     // create struct of data
     strcpy(preference.genre1, genre[g1]);
     preference.genrepref1 = g1;
@@ -109,35 +130,37 @@ struct pref new_rec(void){
     preference.timetowatch = diff;
 
     return preference;
-
 }
 
-struct conpref con_rec(void){
-    char series[50] = "?";
-    do { // Get series name
+conpref con_rec(void)
+{
+    char seriesName[50] = "?";
+    do
+    { // Get series name
         printf("\nWrite the name of the series: \n");
-        scanf(" %49s", &series);
+        scanf(" %49s", &seriesName);
 
-    } while (series == "?");
+    } while (seriesName == "?");
 
     int diff = getdiff(); // Get time diff in seconds
 
-    struct conpref con_series;
+    conpref con_series;
     // create struct of data
-    strcpy(con_series.series, series);
+    strcpy(con_series.seriesName, seriesName);
     con_series.timetowatch = diff;
 }
 
-int getdiff(void){
+int getdiff(void)
+{
 
     time_t time1;
     struct tm stoptimetm;
     int hr, minutes;
     int diff = 0;
 
-    time(&time1); // get current UNIX time
+    time(&time1);                             // get current UNIX time
     struct tm *starttime = localtime(&time1); // convert UNIX to Year, Day, etc.
-    stoptimetm = *starttime; // set stoptime to today
+    stoptimetm = *starttime;                  // set stoptime to today
 
     printf("\nWrite the stoptime (Eg. 18:40): \n");
     scanf("%d:%d", &hr, &minutes);
@@ -148,12 +171,14 @@ int getdiff(void){
 
     time_t stoptime = mktime(&stoptimetm); // convert back to UNIX
 
-    if (difftime(stoptime, time1) <= 0){ // if stop time is next day, add 1 day in seconds
-        diff = difftime(stoptime, time1) + 86400;       
-    } else {
+    if (difftime(stoptime, time1) <= 0)
+    { // if stop time is next day, add 1 day in seconds
+        diff = difftime(stoptime, time1) + 86400;
+    }
+    else
+    {
         diff = difftime(stoptime, time1);
     }
 
     return diff;
-
 }
